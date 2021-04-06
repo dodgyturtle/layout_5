22baf025b521c627897208fb9d3d394e0daac92e
Aleksandr Semenov <semenlinux@gmail.com>
2021-04-06 21:52:01 +0300

    Update variables name

--- render_website.py
+++ render_website.py
@@ -6,54 +6,55 @@ from livereload import Server
 from more_itertools import chunked
 
 
-def load_book_description(filepath: str) -> dict:
-    with open(filepath, "r") as book_description_file:
-        book_description = book_description_file.read()
-    book_description = json.loads(book_description)
-    return book_description
+def load_books_description(file: str) -> dict:
+    with open(file, "r") as books_description_file:
+        books_description_content = books_description_file.read()
+    books_description = json.loads(books_description_content)
+    return books_description
 
 
 def save_to_html(
-    book_description: dict, directorypath: str, filepath_template: str = "template.html"
+    books_description: dict, directory_path: str, template_file: str = "template.html"
 ):
     env = Environment(
-        loader=FileSystemLoader("statics"), autoescape=select_autoescape(["html", "xml"])
+        loader=FileSystemLoader("statics"),
+        autoescape=select_autoescape(["html", "xml"]),
     )
-    template = env.get_template(filepath_template)
-    chunked_books = chunked(book_description, 2)
-    book_index_pages = list(chunked(chunked_books, 20))
-    page_count = len(book_index_pages)
-    for index_filename, book_index_page in enumerate(book_index_pages, 1):
+    template = env.get_template(template_file)
+    chunked_books = chunked(books_description, 2)
+    books_pages = list(chunked(chunked_books, 20))
+    pages_count = len(books_pages)
+    for number, books_page in enumerate(books_pages, 1):
         rendered_page = template.render(
-            chunked_books=book_index_page,
-            page_number=index_filename,
-            page_count=page_count,
+            chunked_books=books_page,
+            page_number=number,
+            pages_count=pages_count,
         )
-        index_filename = f"index{index_filename}.html"
-        index_filepath = os.path.join(directorypath, index_filename)
-        with open(index_filepath, "w", encoding="utf8") as file:
+        html_filename = f"bookspage{number}.html"
+        html_filepath = os.path.join(directory_path, html_filename)
+        with open(html_filepath, "w", encoding="utf8") as file:
             file.write(rendered_page)
 
 
 def on_reload(
     watched_file: str,
-    index_file_directory: str,
+    html_files_directory: str,
     root_directory: str = ".",
-    default_index_filename: str = "statics/pages/index1.html",
-    ):
+    default_html_file: str = "statics/pages/bookspage1.html",
+):
     server = Server()
-    book_description = load_book_description(watched_file)
-    server.watch(watched_file, save_to_html(book_description, index_file_directory))
-    server.serve(root=root_directory, default_filename=default_index_filename)
+    books_description = load_books_description(watched_file)
+    server.watch(watched_file, save_to_html(books_description, html_files_directory))
+    server.serve(root=root_directory, default_filename=default_html_file)
 
 
 def main():
-    index_file_directory = "statics/pages"
-    book_description_filepath = "statics/json/book_desc.json"
-    book_description = load_book_description(book_description_filepath)
-    os.makedirs(index_file_directory, exist_ok=True)
-    save_to_html(book_description, index_file_directory)
-    on_reload(book_description_filepath,index_file_directory=index_file_directory,)
+    html_files_directory = "statics/pages"
+    books_description_file = "statics/json/book_desc.json"
+    books_description = load_books_description(books_description_file)
+    os.makedirs(html_files_directory, exist_ok=True)
+    save_to_html(books_description, html_files_directory)
+    on_reload(books_description_file, html_files_directory=html_files_directory)
 
 
 if __name__ == "__main__":
